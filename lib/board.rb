ed48afbc2116e4bd14df917b1d8053a3c246ae1b
Yasmin Torres <yasmintorres90@hotmail.com>
2018-10-30 20:29:43 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
 
-def board
-  board = Array.new [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def board(Array, index)
+  board [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
